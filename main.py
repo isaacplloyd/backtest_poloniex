8c0f0eb3ed912b46e856757ec53acf008f7fdf37
isaacplloyd <31959039+isaacplloyd@users.noreply.github.com>
2018-02-02 22:33:48 -0500

    Added update function
    changed default directory for CSVs to ./CSVs/
    added 'update' function to console prompt->gets latest price history from polo and appends to pre-existing CSV

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import datetime
+import datetime as dt
+import pandas as pd
 
 from poloniex import dl_history, get_tickers, posix
+from poloniex import update_csv, find_next_timestamp
+from poloniex import ticker_data_exists, prompt_for_date_limits
+
 
 
 def main():
@@ -9,13 +13,13 @@ def main():
     while cmd.lower() != 'exit':    
         cmd = input('cmd: ')
         
-        #dl price history CSV(s) to current directory
+        #dl price history CSV(s) to "./CSVs"
         if cmd.lower() == 'download':
             #Set start date to 20 weeks ago (140 days)
-            now = datetime.datetime.utcnow()
-            history_length = datetime.timedelta(140)
+            now = dt.datetime.utcnow()
+            history_length = dt.timedelta(140)
             start_date = posix(
-                    datetime.datetime(now.year, now.month, now.day, tzinfo=None)
+                    dt.datetime(now.year, now.month, now.day, tzinfo=None)
                     - history_length)
             
             #Get tickers and download histories
@@ -24,13 +28,42 @@ def main():
                 dl_history(symbol, start_date)
             print('download(s) complete!')
         
-        #Update CSVs in program directory
+        #Update CSVs in "./CSVs"
         elif cmd.lower() == 'update':
-            
-            print('coming soon...')
-            
-        else:
+            ticker_symbol_list = get_tickers()
+            for symbol in ticker_symbol_list:
+                filename = "./CSVs/"+symbol+'.csv'
+                df = pd.read_csv(filename)
+                start_date = find_next_timestamp(df)
+                df.set_index('date',inplace=True)
+                update_csv(df, symbol, start_date)
+
+        #Load a dataframe with CSV ticker data from a given time range
+        elif cmd.lower() == 'load':
+            symbol = input('ticker symbol: ')
+            exists, path = ticker_data_exists(symbol)
+            if exists:
+                df = pd.read_csv(path)
+                f_dt, l_dt = date_limits(df)
+                lower_lim, upper_lim = prompt_for_date_limits(f_dt, l_dt)
+                #open_df = get_data_range(path, '...')
+            else:
+                print("./CSVs/"+symbol+'.csv not found.')
+        elif cmd.lower() != 'exit':
             print('cmd not recognized')
 
 if __name__ == "__main__":
     main()
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
--- poloniex.py
+++ poloniex.py
@@ -2,9 +2,99 @@
 various methods to support collecting price histories from the Poloniex
 JSON API
 """
+import datetime as dt
 import datetime
 import pandas as pd
 import os.path
+
+def prompt_for_date_limits(f_dt, l_dt):
+    print('oldest data is dated',dt.datetime.strftime(f_dt, '%Y-%m-%d %H:%M:%S'))
+    print('latest data is dated',dt.datetime.strftime(l_dt, '%Y-%m-%d %H:%M:%S'))
+    in_ll = input("""enter first date in "yyyy,m,d,h,m" format: """)
+    print('soorry!  load function not supported yet... try out update! it works...')
+    return datetime.datetime(2018,1,1,0,0), datetime.datetime(2018,1,1,0,0)
+
+    #if...
+
+def ticker_data_exists(symbol):
+    '''
+    Check for existence of CSV file for symbol
+    param:      symbol; string
+    out ->      bool; file exists
+    out ->      string; file path
+    '''
+    filename = "./CSVs/"+symbol+'.csv'
+    return os.path.isfile(filename), filename
+    
+def update_csv(df_in, symbol, start_date, end_date='9999999999', period=300):
+    '''
+    Requests latest price history on 'symbol'.
+    Accepts current data, appends new data, then overwrites old CSV.
+    param:		df_in; pd.dataframe.DataFrame object; data from current CSV
+    param: 		symbol; string
+    param: 		start_date; string
+    [param]:	   end_date; string = '9999999999'
+    [param]:    period; int = 300 (seconds)
+    out -> 		bool; "operation successful" (file updated)
+    '''
+
+    #Get JSON data
+    url = get_url(symbol, start_date, end_date, period)
+    try:
+        df = pd.read_json(url).set_index('date')
+    except:
+        print('Error processing URL:', url)
+        print()
+        print('Are you sure that pair exists?')
+        return False
+
+    #Append new data and overwrite old CSV
+    try:
+        filename = "./CSVs/"+symbol+'.csv'
+        df_out = df_in.append(df).dropna()
+        os.remove(filename)
+        df_out.to_csv(filename)
+        print('Updated:', symbol)
+    except:
+        print('Error overwriting',filename)
+        return False
+    else:
+        return True
+
+def date_limits(df):
+    '''
+    Returns first and last dates in dataframe
+    param:      df; pd.dataframe.DataFrame object with ticker history data
+    out ->      datetime.DateTime; first date in dataframe
+    out ->      datetime.DateTime; last date in dataframe
+    '''
+    first_date = df['date'][0]
+    first_dt = datetime.datetime.strptime(first_date, '%Y-%m-%d %H:%M:%S')
+    
+    last_index = (df['date'].size-1)
+    last_date = df['date'][last_index]
+    last_dt = datetime.datetime.strptime(last_date, '%Y-%m-%d %H:%M:%S')
+
+    return first_dt, last_dt
+    
+def find_next_timestamp(df):
+    #Params: symbol; string represents currency pair, e.g. 'BTC_XRP' is
+    #        the bitcoin/ripple pair
+    #Returns: string; POSIX timestamp that's 5 minutes later than the
+    #               newest data in the file.  Intended to be used directly
+    #               as the start date for a JSON price history request
+    #               from Poloniex
+    
+    #Get last date from dataframe and add 5 minutes to get next timestamp
+    last_index = (df['date'].size-1)
+    last_date = (df['date'][last_index])
+    last_dt = datetime.datetime.strptime(last_date, '%Y-%m-%d %H:%M:%S')
+    five_min = datetime.timedelta(minutes=5)
+    next_dt = last_dt + five_min
+        
+    #convert next timestamp from datetime.datetime object to POSIX string
+    next_dt_posix = posix(next_dt)
+    return next_dt_posix
 
 def dl_history(symbol, start_date, end_date='9999999999', period=300,):
     '''
@@ -17,7 +107,7 @@ def dl_history(symbol, start_date, end_date='9999999999', period=300,):
         valid values: 300, 900, 1800, 7200, 14400, and 86400
     Returns: bool True if data is pulled and successfully written
     '''
-    if os.path.isfile(symbol+'.csv'):
+    if os.path.isfile("./CSVs/"+symbol+'.csv'):
         print(symbol,'.csv already exists.  Delete file or use update_csv instead.')
         return False
     
@@ -26,7 +116,7 @@ def dl_history(symbol, start_date, end_date='9999999999', period=300,):
     try:
         df = pd.read_json(url)
         df.set_index('date',inplace=True)
-        df.to_csv(symbol + '.csv')
+        df.to_csv("./CSVs/"+symbol + '.csv')
         print('Processed:',symbol)
     except:
         print('Error processing URL:',url)
